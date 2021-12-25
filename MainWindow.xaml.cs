f954b234c5b67c943aece3f5aac1dee47e290928
vorvit <94832434+vorvit@users.noreply.github.com>
2021-12-25 04:19:32 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF14
+{
+    /*ЗАДАНИЕ 14. ШАБЛОНЫ ДАННЫХ
+        Определить класс Product для хранения информации о товаре. Класс должен содержать характеристики Название товара (строка), Цена (число),
+        Изображение (строка - путь к файлу с изображением), Категория (перечисление, возможные значения – Еда, Бытовая техника).
+        Разработать шаблон данных для отображения списка товаров в компоненте ListBox. 
+        При помощи триггеров определите разный шаблон для товаров категории Еда и категории Бытовая техника. 
+     */
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProdName = "Бананы",
+                ProdPrice = 70,
+                ProdImage = "Data/Бананы.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            { 
+                ProdName = "Кокосы",
+                ProdPrice = 120,
+                ProdImage = "Data/Кокосы.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Ананасы",
+                ProdPrice = 260,
+                ProdImage = "Data/Ананасы.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Пылесос",
+                ProdPrice = 7000,
+                ProdImage = "Data/Пылесос.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Утюг",
+                ProdPrice = 3500,
+                ProdImage = "Data/Утюг.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Стиральная машина",
+                ProdPrice = 35000,
+                ProdImage = "Data/Стиральная_машина.jpg",
+                ProductCategory = ProductCategorys.Appliances
+            });
+            listBox.ItemsSource = products; 
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF14
+{    /*ЗАДАНИЕ 14. ШАБЛОНЫ ДАННЫХ
+        Определить класс Product для хранения информации о товаре. Класс должен содержать характеристики Название товара (строка), Цена (число),
+        Изображение (строка - путь к файлу с изображением), Категория (перечисление, возможные значения – Еда, Бытовая техника).
+        Разработать шаблон данных для отображения списка товаров в компоненте ListBox. 
+        При помощи триггеров определите разный шаблон для товаров категории Еда и категории Бытовая техника. 
+     */
+    public enum ProductCategorys
+    {
+        Food,
+        Appliances
+    }
+    
+    public class Product
+    {
+        public string ProdName{ get; set; }
+        public decimal ProdPrice{ get; set; }
+        public string ProdImage{ get; set; }
+        public ProductCategorys ProductCategory{ get; set; }
+
+        public override string ToString()
+        {
+            return ProdName;
+        }
+    }
+
+
+}
